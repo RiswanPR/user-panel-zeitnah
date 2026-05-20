dd9ff13c25ba73f0fe52900511def457acff08b2
RiswanPR <prriswan72@gmail.com>
2026-05-20 17:27:54 +0530

    updated backend frontend

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,12 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AuthModule } from './auth/auth.module';
-import { UsersModule } from './users/users.module';
-import { PostsModule } from './posts/posts.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { UsersModule } from './modules/users/users.module';
+import { PostsModule } from './modules/posts/posts.module';
+import { MongooseModule } from '@nestjs/mongoose';
+
 
 @Module({
-  imports: [AuthModule, UsersModule, PostsModule],
+  imports: [
+    AuthModule,
+    UsersModule,
+    PostsModule,
+    MongooseModule.forRoot('mongodb://127.0.0.1:27017/lms-platform'),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
